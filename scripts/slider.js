87b26bc5c70ecbd0116feb913aa2a5857c14eea8
Zhuldyz Akhmetova <84645560+JuldyzA@users.noreply.github.com>
2026-01-09 13:31:36 -0800

    Organized slider.css and aligned layout in market and science sections

--- scripts/slider.js
+++ scripts/slider.js
@@ -3,7 +3,7 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("auto-slide");
+  var x = document.getElementsByClassName("banner-slide");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
